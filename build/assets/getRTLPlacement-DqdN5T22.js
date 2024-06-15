c689f1c581b0e3b349092e0815dfedcc4d053485
yanouyehiel <yanou.yehiel@yahoo.com>
2024-06-15 23:32:15 +0200

    Modification du fichier .gitignore

---
+++ build/assets/getRTLPlacement-DqdN5T22.js
@@ -0,0 +1,1 @@
+import{x as e}from"./DefaultLayout-qFvcpqxs.js";var a=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{a as g};
